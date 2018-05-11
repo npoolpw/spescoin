772ac48c09650ddce6ea017c8568504d14761d68
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-05-11 08:56:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var api = "http://ca.npool.pw:2260";
+var api = "http://sg2.npool.pw:2260";
 
-var poolHost = "ca.npool.pw";
+var poolHost = "sg2.npool.pw";
 
 var blockExplorer = "http://blockexplorer.pw/coin";
 
@@ -8,13 +8,13 @@ var cryptonatorWidget = ["BTC-USD", "SPES-BTC"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://pool.myspes.org/?hash={symbol}/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://pool.myspes.org/?hash={symbol}/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["npool.pw/spescoin", "http://ca.npool.pw:2260"],
+        ["npool.pw/spescoin", "http://sg2.npool.pw:2260"],
         ["pool.myspes.org", "http://pool.myspes.org:8133"]
 ];
